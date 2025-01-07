fd23aa881223716872427acd8d4c9cac6d964758
mdmelo <melomiked@yahoo.com>
2025-01-07 08:12:54 -0500

    fix simple app loop for task demo

--- assist01.py
+++ assist01.py
@@ -1,3 +1,6 @@
+import sys
+import time
+
 import speech_recognition as sr
 import numpy as np
 
@@ -8,10 +11,9 @@ from io import BytesIO
 from queue import Queue
 from moonif import moonshine_main_live_audio
 
-import pyautogui
-import time
 
-
+tasks = []
+listeningToTask = False
 
 _debug = True
 
@@ -56,62 +58,89 @@ def listen_for_command():
         print("Unable to access the Google Speech Recognition API.")
         return None
 
-def respond(response_text, delay=0):
-    if _debug: print("TTS>>> ", response_text)
+
+def respond(response_text, delay=2):
+    if _debug:
+        print("TTS>>> ", response_text)
     speak(response_text, lang='en', play=True, delay=delay)
-
-tasks = []
-listeningToTask = False
-
-
 
 
 def main():
     global tasks
     global listeningToTask
 
+    triggerKeyword = "wahoo"
+    command = ""
+
+    while command != triggerKeyword:
+        command = listen_for_command()
+
     while True:
         command = listen_for_command()
 
-        # XXX FIX ME
-        triggerKeyword = "wahoo"
-
-        if command and triggerKeyword in command:
+        if command:
             if listeningToTask:
                 tasks.append(command)
                 listeningToTask = False
                 respond("Adding " + command + " to your task list. You have " + str(len(tasks)) + " currently in your list.")
+
             elif "add a task" in command:
                 listeningToTask = True
-                respond("Sure, what is the task?")
+                respond("What is the task?")
+
             elif "list tasks" in command:
-                respond("Sure. Your tasks are:")
+                respond("Your tasks are:")
                 for task in tasks:
                     respond(task)
-            elif "take a screenshot" in command:
-                pyautogui.screenshot("screenshot.png")
-                respond("I took a screenshot for you.")
-            elif "open chrome" in command:
-                respond("Opening Chrome.")
-                webbrowser.open("http://www.youtube.com/@JakeEh")
+
             elif "exit" in command:
                 respond("Goodbye!")
                 break
+
             else:
                 respond("Sorry, I'm not sure how to handle that command.")
 
 
 def process_command(text):
-    print("\nMoonshine VR supplies '{}'".format(text))
+    global tasks
+    global listeningToTask
+
+    if _debug:
+        print("\nMoonshine VR supplies '{}'".format(text))
 
     # consider using distance calc for word matching
     # see /media/mike/MMBKUPDRV/yottaa/Books/MachineLearning/Voice/voice-control/spoken-command-processor/processor/utils.py
 
+    if text:
+        command = text.casefold()
+
+        if listeningToTask:
+            tasks.append(command)
+            respond("Sure, adding " + command + " to your task list")
+            respond("You have " + str(len(tasks)) + " tasks now.")
+            listeningToTask = False
+
+        elif "add task" in command:
+            listeningToTask = True
+            respond("Sure, what is the task?")
+
+        elif "list task" in command:
+            respond("Sure, your tasks are:")
+            for task in tasks:
+                respond(task)
+
+        elif "exit" in command:
+            respond("Goodbye!")
+            sys.exit(0)
+
+        else:
+            respond("Sorry, I'm not sure how to handle that command.")
+
 
 if __name__ == "__main__":
-    respond("Greetings, Wahoo!", 2)
+    respond("Greetings, Wahoo!")
 
     moonshine_main_live_audio(cbfunc=process_command, model_name="moonshine/base")
-    respond("Goodbye, Wahoo!", 2)
+    respond("Goodbye, Wahoo!")
 
 
